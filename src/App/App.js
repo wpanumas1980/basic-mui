c116c08502f7d6a87b9e94cc90f946e108818987
wpanumas1980 <wpanumas@gmail.com>
2021-01-18 18:16:58 +0700

    Mui Theme Customization

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,64 @@
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import Header from '../components/Header';
+import PageHeader from '../components/PageHeader';
 import SideMenu from '../components/SideMenu';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FFF000',
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: '#FFF444',
+      light: '#f84526'
+    },
+    background: {
+      default: '#f4f4fd'
+    }
+  },
+  overrides:{
+    MuiAppBar:{
+      root:{
+        transform:'translateZ(0)'
+      }
+    }
+  },
+  props:{
+    MuiIconButton:{
+      disableRipple:'true'
+    }
+  }
+  // shape:{
+  //   borderRadius:'12px'
+  // }
+})
 
 const useStyles = makeStyles({
-  appMain :{
-    paddingLeft :'320px',
-    width:'100%'
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
   }
 })
+
 function App() {
   const classes = useStyles()
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
-        <Header/>
+        <Header />
+        <PageHeader
+          title="Page Header"
+          subTitle="Page Description"
+          icon={<PeopleOutlineTwoToneIcon fontSize="large"/>}
+        />
       </div>
-      <CssBaseline/>
-    </>
+      <CssBaseline />
+    </ThemeProvider >
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,6 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  Container,
   Grid,
   InputBase,
   Badge
@@ -15,33 +11,25 @@ import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import SearchIcon from '@material-ui/icons/Search';
-import { Home } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   searchInput: {
     opacity: '0.6',
-    padding: '0px 8px',
+    padding: `0px ${theme.spacing(1)}`,
     fontSize: '0.8rem',
     '&:hover':{
       backgroundColor:'#f2f2f2'
     },
     '& .MuiSvgIcon-root':{
-      marginRight:'8px',
+      marginRight:theme.spacing(1),
     }
   }
-});
+}));
 
-const navLinks = [
-  { title: `about us`, path: `/about-us` },
-  { title: `product`, path: `/product` },
-  { title: `blog`, path: `/blog` },
-  { title: `contact`, path: `/contact` },
-  { title: `faq`, path: `/faq` }
-];
 
 const Header = () => {
   const classes = useStyles();
@@ -59,7 +47,7 @@ const Header = () => {
           </Grid>
           <Grid item sm></Grid>
           <Grid item >
-            <IconButton classes={{root:classes.btnRoot,label:classes.btnLabel}}>
+            <IconButton >
               <Badge badgeContent={4} color="secondary">
                 <NotificationsNoneIcon fontSize="small" />
               </Badge>
